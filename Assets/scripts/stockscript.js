f6b9e08f924e10fa4dc0bc9bceb37bad17be8afc
jmobrien1976 <jmobrien1@hotmail.com>
2022-08-18 10:05:49 -0600

    commit to pull

--- Assets/scripts/stockscript.js
+++ Assets/scripts/stockscript.js
@@ -8,9 +8,8 @@ const apikey = "031790bc33587193255896a4a8034319"
 var highOfDay = document.querySelector('#high');
 var lowOfDay = document.querySelector('#low');
 var current = document.querySelector('#current');
-var companyName = "";
-var requestedQuote = "AAPL"
-
+// var companyName = "";
+var requestedQuote = "ABC"
 
 $.ajax({
     url: url + requestedQuote + "?apikey=" + apikey,
@@ -18,15 +17,17 @@ $.ajax({
 })
 .then(function (data) {
     console.log(data)
-
+    companyName.text = "Company Name:  " + data[0].name;
     high.textContent = "High of Day:  " + data[0].dayHigh;
     low.textContent = "Low of Day:  " + data[0].dayLow;
     current.textContent = "Current Price:  " + data[0].price;
-    companyName.text = "Company Name:  " + data[0].name;
+
   });
 
 
 // high of day 
+// low of day 
+// current price 
 // low of day 
 // current price 
 
@@ -47,24 +48,37 @@ const addAnotherRow = () => {
 
 const key = "031790bc33587193255896a4a8034319";
 
-const beforeUrl = `https://financialmodelingprep.com/api/v3/quote/`;
+const beforeUrl = "https://financialmodelingprep.com/api/v3/quote/";
 
-$("#submit").on('click', (e) => {
-    e.preventDefault();
-    const allSymbols = document.querySelectorAll(".pQuery");
 
-    for (let i = 0; i < allSymbols.length; i++) {
-        symbolList.push(allSymbols[i].textContent.toUpperCase());
-        $.ajax({
-            url : `${beforeUrl}${allSymbols[i].textContent.toUpperCase()}?apikey=${key}`,
-            method: "GET"
-        }).then ( (value) => {
-            console.log(value);
-        
-        }).catch( (error) => {
-            console.log(error);
-        });
-    }});
+const createTableResult = () => {
+    
+}
+
+
+
+
+// $("#submit").on('click', (e) => {
+//     e.preventDefault();
+//     const allSymbols = document.querySelectorAll(".pQuery");
+
+//     for (let i = 0; i < allSymbols.length; i++) {
+//         symbolList.push(allSymbols[i].textContent.toUpperCase());
+//         $.ajax({
+//             url : `${beforeUrl}${allSymbols[i].textContent.toUpperCase()}?apikey=${key}`,
+//             method: "GET"
+//         }).then ( (value) => {
+//             console.log(value);
+//             requestedQuote.coName[i] = value[0].name;
+//             requestedQuote.high[i] = value[0].dayHigh;
+//             requestedQuote.low[i] = value[0].dayLow;
+//             requestedQuote.current[i] = value[0].price;
+//             console.log(requestedQuote[i].coName)
+
+//         }).catch( (error) => {
+//             console.log(error);
+//         });
+//     }});
 
 
 const queryHolder = $(".queryHolder");
@@ -87,16 +101,3 @@ $("#addMore").on('click', (e) => {
     const allSymbols = document.querySelector("#symbolInput");
     pushQuery(allSymbols.value);
 });
-
-$("#submit").on('click', (e) => {
-    e.preventDefault();
-    const allSymbols = document.querySelectorAll("#symbolInput");    
-    for (let i = 0; i < allSymbols.length; i++) {
-      if (allSymbols[i].value != "") {
-            symbolList.push(allSymbols[i].value.toUpperCase());
-        }
-    }
-})
-
-
-
